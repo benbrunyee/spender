b8489e35a19c14242b21dee463692eaf9c0c2b4e
bacon1097 <brunyeeb@gmail.com>
2023-05-01 12:35:41 +0100

    initial commit

---
+++ src/firebase.ts
@@ -0,0 +1,21 @@
+import { initializeApp } from 'firebase/app';
+import { connectFirestoreEmulator, getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+	apiKey: 'AIzaSyDXhUhH15Lzz4hqsgvPjfoJimZjRoWE8Zo',
+	authDomain: 'spender-7e181.firebaseapp.com',
+	projectId: 'spender-7e181',
+	storageBucket: 'spender-7e181.appspot.com',
+	messagingSenderId: '578896377696',
+	appId: '1:578896377696:web:3a9d82323e696c29e95005'
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const firestore = getFirestore(app);
+
+if (import.meta.env.DEV) {
+	connectFirestoreEmulator(firestore, 'localhost', 8080);
+}
+
+export { app, firestore };
